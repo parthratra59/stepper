c69f0a07f5139bda45bc01985a6663e7a90aabf4
Parth Ratra <ratraparth59@gmail.com>
2024-05-18 20:10:46 +0530

    Update CheckoutStepper.tsx

--- src/utils/CheckoutStepper/CheckoutStepper.tsx
+++ src/utils/CheckoutStepper/CheckoutStepper.tsx
@@ -1,24 +1,33 @@
 import React, { useEffect, useRef, useState, useContext } from "react";
 import { stepContext } from "../../context/StepContext";
 
+// Define the interface for the step configuration
 interface StepConfig {
   name: string;
   component: React.ComponentType<[]>;
 }
 
+// Define the interface for the props of the CheckoutStepper component
 interface CheckoutStepperProps {
   stepsConfig: StepConfig[];
 }
-
+// Define the CheckoutStepper component that will render the stepper UI
 const CheckoutStepper: React.FC<CheckoutStepperProps> = ({ stepsConfig = [] }) => {
+  // Extract the current step from the context
   const { Currstep } = useContext(stepContext);
+
+  
+    // State to manage the left and right margins of the progress bar
   const [margins, setMargins] = useState<{ marginLeft: number; marginRight: number }>({
     marginLeft: 0,
     marginRight: 0,
   });
 
+   // Ref to store references to each step element
   const stepRef = useRef<HTMLElement[]>([]);
 
+
+  // useEffect to calculate margins for the first and last steps
   useEffect(() => {
     if (stepsConfig.length > 0) {
       setMargins({
@@ -31,7 +40,7 @@ const CheckoutStepper: React.FC<CheckoutStepperProps> = ({ stepsConfig = [] }) =
   }, [stepsConfig.length]);
 
 
-
+// Function to calculate the width of the progress bar based on the current step
   const calculateProgressBarWidth = () => {
     return ((Currstep - 1) / (stepsConfig.length - 1)) * 100;
   };

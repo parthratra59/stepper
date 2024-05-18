b5ab10a1de4500e77bb9a41a597ae4d4e9ce2039
Parth Ratra <ratraparth59@gmail.com>
2024-05-18 20:03:08 +0530

    Update userRoute.ts

--- stepper_server/src/routes/userRoute.ts
+++ stepper_server/src/routes/userRoute.ts
@@ -1,11 +1,12 @@
 import express from "express";
-
+// Create a new router instance
 const router = express.Router();
 
 import createUser  from "../Controllers/User_Controller";
 
-
+// Define a POST route to create a new user
+// When a POST request is made to "/createUser", the createUser controller function is called
 router.post("/createUser",createUser)
 
-
+// Export the router to be used in other parts of the application
 export default router;

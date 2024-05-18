6c6d771294ccec514ada30e0ed707df526f1fcb2
Parth Ratra <ratraparth59@gmail.com>
2024-05-18 20:00:27 +0530

    Update db.config.ts

--- stepper_server/src/db/db.config.ts
+++ stepper_server/src/db/db.config.ts
@@ -1,13 +1,14 @@
 
 import { PrismaClient } from "@prisma/client";
 
-// Creating a new instance of PrismaClient
+// Creating a new instance of PrismaClient with logging options enabled
+// The log option allows capturing various events (queries, info, warnings, and errors) for better debugging and monitoring.
 const prisma = new PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
   });
 
 
- 
+ // Exporting the PrismaClient instance for use in other parts of the application
 export default prisma;
 
 

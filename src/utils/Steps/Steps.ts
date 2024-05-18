2474761b4d0939c4efe8a5d2fe56cacb24287f76
Parth Ratra <ratraparth59@gmail.com>
2024-05-18 20:12:06 +0530

    Update Steps.ts\

--- src/utils/Steps/Steps.ts
+++ src/utils/Steps/Steps.ts
@@ -2,19 +2,19 @@ import PersonalDetails from "../../components/Personal_Details/Personal_Details"
 import Payment from "../../components/Payment/Payment";
 import ThankYouComponent from "../../components/Thankyou_Component/Thankyou_Component";
 
-
+// Define the steps for the stepper with their names and corresponding components
 const Steps = [
   {
       name: "User Details",
-      component: PersonalDetails
-  },
+      component: PersonalDetails  // Component to collect personal details from the user
+  }, 
   {
       name: "Payment",
-      component: ThankYouComponent
+      component: ThankYouComponent  // Component to handle payment details and processing
   },
   {
       name: "ThankYou",
-      component: Payment
+      component: Payment   // Component to display a thank you message after successful completion
   }
 ];
 

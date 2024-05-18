694b7c7e43d4a704c037741ea76c211f954272d0
Parth Ratra <ratraparth59@gmail.com>
2024-05-18 20:08:25 +0530

    Update StepProvider.tsx

--- src/context/StepProvider.tsx
+++ src/context/StepProvider.tsx
@@ -1,6 +1,7 @@
 import {  useState } from "react";
 import { stepContext } from "./StepContext";
 
+// Define the interface for the step data
 interface stepDataProvider {
   fullName: string;
   email: string;
@@ -15,11 +16,11 @@ interface stepDataProvider {
 
 
 
-
+// Define the interface for the props of the StepProvider component
 interface stepProviderProps {
   children: React.ReactNode;
 }
-
+// Define the StepProvider component that will provide the context to its children
 const StepProvider: React.FC<stepProviderProps> = ({ children }) => {
   const [stepData, setStepData] = useState<stepDataProvider>({
     fullName: "",
@@ -37,19 +38,24 @@ const StepProvider: React.FC<stepProviderProps> = ({ children }) => {
 
 
 
-
+// State to manage the current step
   const [Currstep, setCurrStep] = useState(1);
-
+ // State to manage the completion status
   const [complete, setComplete] = useState(false);
+   // State to manage the visibility of the thank you message
   const [showThankYou, setShowThankYou] = useState(false);
+   // State to manage the required fields' filled status
   const [requiredfilled, setRequiredfilled] = useState(false);
+  // State to manage if the phone number has been edited
   const [phoneEdited, setPhoneEdited] = useState<boolean>(false);
 
-  // card page
+  
+   // States to manage card page validations
   const [validcarddigit, setValidcarddigit] = useState(false);
   const [validcvv, setValidcvv] = useState(false);
   const [expiryDate, setExpiryDate] = useState(false);
 
+  // The value to be provided to the context consumers
   const value = {
     stepData,
     setStepData,
@@ -71,6 +77,7 @@ const StepProvider: React.FC<stepProviderProps> = ({ children }) => {
     setExpiryDate,
   };
 
+   // Return the context provider with the value and children
   return <stepContext.Provider value={value}>{children}</stepContext.Provider>;
 };
 
